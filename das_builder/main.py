995c0a5d2a9dcca6984309b7e3edc64dcd0f97a0
brandon phelps <bwp44f@mst.edu>
2020-12-26 12:23:55 -0600

    added in a bit of a skelyton

--- das_builder/main.py
+++ das_builder/main.py
@@ -2,23 +2,50 @@
 import docker
 import os
 from  jinja2 import Environment, FileSystemLoader
+import argparse
+
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class RunnerArgs:
+    pass
+
+def search_for_root():
+    """
+    return the path containing the configuration file to read from for loading up and running. 
+    """
+    
+def extract_args(config_file):
+    """
+    Read from the config file and pull out the intresting arguments to pass to the execu line
+    """
+    
+
 
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image', help="name of docker image to use")
+
+    args = parser.parse_args()
+
+    
+    
+
     current_dir = os.path.abspath(os.getcwd())
     client = docker.from_env()
-    print("Current dir: {}", current_dir)
-
     j2_env = Environment(loader=FileSystemLoader(os.path.join(THIS_DIR, 'templates')),
                          trim_blocks=True)
 
     with open(os.path.join(current_dir, 'docker_builder.sh'), 'w') as writer:
         writer.write(j2_env.get_template('conan_build.sh').render())
 
-    print("Running container")
-    cont = client.containers.run("conanio/gcc7", command=["bash", "docker_builder.sh"], volumes={current_dir: {"bind": "/work_dir", "mode" : "rw"}}, working_dir="/work_dir", detach=True)
-    
-    print(cont.logs())
+    cont = client.containers.run(args.image,
+                                 command=["bash", "docker_builder.sh"],
+                                 volumes={current_dir: {"bind": "/work_dir",
+                                                        "mode" : "rw"}},
+                                 working_dir="/work_dir", detach=True)
+    for i in cont.output:
+        print(i)
+
