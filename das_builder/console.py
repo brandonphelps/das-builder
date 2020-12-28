46854546ab7beb00b486a99a1b4157527dbabc27
brandonphelps <brandonphelps>
2020-12-28 21:33:50 +0000

    output occurs now? wasn't seeing it before

--- das_builder/console.py
+++ das_builder/console.py
@@ -1,4 +1,5 @@
 import docker
+import time
 import os
 from jinja2 import Environment, FileSystemLoader
 import argparse
@@ -15,22 +16,13 @@ def run_build(client, image, command):
     pass
 
 
-def chown_dir(root_path, uid, gid):
-    for root, dirs, files in os.walk(root_path):
-        for d in dirs:
-            os.chown(os.path.join(root, d), uid=uid, gid=gid)
-
-        for f in files:
-            os.chown(os.path.join(root, f), uid=uid, gid=gid)
-
-
 def main():
     current_dir = search_for_root(os.getcwd())
     config = load_config(os.path.join(current_dir, CONFIG_FILE_NAME))
 
     image_name = config["das-builder"]["image"]
 
-    client = docker.from_env()
+
     j2_env = Environment(
         loader=FileSystemLoader(os.path.join(THIS_DIR, "templates")), trim_blocks=True
     )
@@ -41,22 +33,28 @@ def main():
         os.path.join(current_dir, ".das_builder", "docker_builder.sh"), "w"
     ) as writer:
         writer.write(
-            j2_env.get_template("conan_build.sh").render(image_name=image_name,
-                                                         uid=os.getuid(),
-                                                         gid=os.getgid())
+            j2_env.get_template("echo.sh").render(image_name=image_name,
+                                                  uid=os.getuid(),
+                                                  gid=os.getgid())
         )
+
+    client = docker.from_env()
+    cmd = ["/bin/bash", os.path.join(".das_builder", "docker_builder.sh")]
 
     cont = client.containers.run(
         image_name,
         # todo: allow user direct command pass through?
         # path join generates host system file paths not guest system.
-        command=["/bin/bash", os.path.join(".das_builder", "docker_builder.sh")],
-        volumes={current_dir: {"bind": "/work_dir", "mode": "rw"}},
-        working_dir="/work_dir",
-        detach=True,
+        command = cmd,
+        volumes = {current_dir: {"bind": "/work_dir", "mode": "rw"}},
+        working_dir = "/work_dir",
+        detach = True,
     )
-    cont.wait()
+
+    t = cont.wait()
+    print("Logs from container")
     print(cont.logs().decode("utf-8"))
+
 
     # todo: need to obtain the output directory from the build process
     # chown_dir(f"build/{image_name}", uid=os.getuid(), gid=os.getgid())
--- das_builder/templates/conan_build.sh
+++ das_builder/templates/conan_build.sh
@@ -3,4 +3,5 @@
 conan install . -if build/{{ image_name }}
 conan build . -bf build/{{ image_name }}
 
-chown -R {{ uid }}:{{ gid }} build/{{ image_name }}
+chown -R {{ uid }}:{{ gid }} build/
+sleep 5
